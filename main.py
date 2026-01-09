5cb8f158208e729700db247d027b9caa8ea27a32
Yasir <147266162+Blazikener@users.noreply.github.com>
2026-01-09 22:42:00 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import pygame, sys
-from settings import * 
-from debug import debug
+from settings import *
 from level import Level
 
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.clock = pygame.time.Clock()
-        pygame.display.set_caption("وحش")
+	def __init__(self):
 
-        self.level = Level()
+		# general setup
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+		pygame.display.set_caption('Zelda')
+		self.clock = pygame.time.Clock()
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+		self.level = Level()
 
-            self.screen.fill('black')
-            self.level.run()
-            pygame.display.update()
-            self.clock.tick(FPS)
+		# sound 
+		main_sound = pygame.mixer.Sound('../audio/main.ogg')
+		main_sound.set_volume(0.5)
+		main_sound.play(loops = -1)
+	
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_m:
+						self.level.toggle_menu()
 
-if __name__ == "__main__":
-    game = Game()
-    game.run()
+			self.screen.fill(WATER_COLOR)
+			self.level.run()
+			pygame.display.update()
+			self.clock.tick(FPS)
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
